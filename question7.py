c54ab454e901fbe853455740b576f245eda13636
İlke Elvan <ilke-pop@pop-os.localdomain>
2020-08-06 19:08:07 +0300

    Change Knapsack solution to Unbounded Knapsack solution.

--- question7.py
+++ question7.py
@@ -1,26 +1,26 @@
 def get_max_value(capacity, carrot_types):
-    K = [[0 for x in range(capacity + 1)] for x in range(len(carrot_types) + 1)]
     carrot_weights = list(carrot_types.keys())
     carrot_prices = list(carrot_types.values())
 
-    for i in range(len(carrot_types) + 1):
-        for w in range(capacity + 1):
-            if i == 0 or w == 0:
-                K[i][w] = 0
-            elif carrot_weights[i-1] <= w:
-                K[i][w] = max(carrot_prices[i-1] + K[i-1][w-carrot_weights[i-1]],  K[i-1][w])
-            else:
-                K[i][w] = K[i-1][w]
+    K = (capacity + 1) * [0]
 
-    return K[len(carrot_types)][capacity]
+    for i in range(capacity + 1):
+        for j in range(len(carrot_types)):
+            if carrot_weights[j] <= i:
+                K[i] = max(K[i], K[i-carrot_weights[j]] + carrot_prices[j])
+
+    return K[capacity]
 
 
-val = [60, 100, 120]
-wt = [10, 20, 30]
+
 bag1 = {5: 100, 7: 150, 3: 70}
 capacity1 = 36
 print(get_max_value(capacity1, bag1))
 
-bag2 = {10: 60, 20: 100, 30: 120}
+bag2 = {10: 40, 20: 100, 30: 120}
 capacity2 = 50
 print(get_max_value(capacity2, bag2))
+
+bag3 = {10: 500, 20: 100, 30: 120}
+capacity3 = 50
+print(get_max_value(capacity3, bag3))
