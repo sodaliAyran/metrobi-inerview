25332538ebfe714f12f01e91aca8aa496e939797
İlke Elvan <ilke-pop@pop-os.localdomain>
2020-08-06 14:43:30 +0300

    Fix typo

--- question1.py
+++ question1.py
@@ -1,6 +1,8 @@
-# Since the list can contain anything ranging from strings, intergers to other list we can't trust the elements to be hashable so we can not use dictionaries and sets therefore we have to resort to a quadratic time solution
+# Since the list can contain anything ranging from strings, integers to other
+# lists we can't trust the elements to be hashable so we can not use
+# dictionaries and sets therefore we have to resort to a quadratic time solution
 
 a = [[1, 2], 2, [2], [3], [1, 2], [5],"ilke", "elvan" , "ilke", [3]]
 
 duplicates = [x for n, x in enumerate(a) if x in a[:n]]
-print(duplicates)	
+print(duplicates)
