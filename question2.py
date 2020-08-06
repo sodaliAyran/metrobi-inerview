ff4c2a58d67298d3bcd7037bc83283bc85e12ff8
İlke Elvan <ilke-pop@pop-os.localdomain>
2020-08-06 13:11:30 +0300

    Add answer 2

---
+++ question2.py
@@ -0,0 +1,20 @@
+# TBH I'm not sure if this is true here is my answer
+
+import asyncio
+import time
+
+
+async def say_after(delay, what):
+    await asyncio.sleep(delay)
+    print(what)
+
+async def main():
+    l = ["a", "b", "c", "d"]
+    delay = 1
+    for item in l:
+        print(f"started at {time.strftime('%X')}")
+        await say_after(delay, item)
+        print(f"finished at {time.strftime('%X')}\n")
+        delay = delay * 2
+
+asyncio.run(main())
