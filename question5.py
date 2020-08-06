9ad4706ea88cf893beaf46090da68c49cf389956
İlke Elvan <ilke-pop@pop-os.localdomain>
2020-08-06 12:57:31 +0300

    Add answer 5

---
+++ question5.py
@@ -0,0 +1,36 @@
+# Worst case scenario is 14 drops which is the solution to the given equation:
+# x * (x+1)/2 = 100
+# Therefore I need to start dropping eggs from 14th floor and continue
+# You can find a good explanation to this here: https://medium.com/@khopsickle/2-eggs-and-100-floors-a032beb77aaa#:~:text=A%20building%20has%20100%20floors,can%20drop%20the%20egg%20again.
+
+import random
+
+FLOOR_COUNT = 100
+egg_breaking_floor = random.randint(1, FLOOR_COUNT+1) # It starts from 1 because you can't(shouldn't) break the egg at the floor 0, you just put it down like a normal person
+
+# As I stated above I start dropping from floor 14 but this can be generalized.
+
+def drop_egg(break_point):
+    min_floor = 0
+    step_size = 14
+    max_floor = step_size
+
+    while(max_floor < FLOOR_COUNT):
+        if max_floor >= break_point:
+            for i in range(min_floor, max_floor):
+                if i >= break_point:
+                    return i - 1
+        else:
+            min_floor = max_floor + 1
+            max_floor = max_floor + step_size - 1
+            step_size = step_size - 1
+
+    if min_floor >= break_point:
+        return min_floor - 1
+    else:
+        return FLOOR_COUNT
+
+print("For break_point 1 answer is: " + str(drop_egg(1)))
+print("For break_point 100 answer is: " + str(drop_egg(100)))
+print("For break_point 101 answer is: " + str(drop_egg(101)))
+print("For break_point 61 answer is: " + str(drop_egg(61)))
